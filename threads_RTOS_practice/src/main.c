d367918444c38e667272152d47c5a250dbabb701
denny61302 <denny61302@gmail.com>
2022-06-23 16:06:18 +0930

    Update main.c
    Queue Example (Buggy!!)
    
    https://www.digikey.com.au/en/maker/projects/introduction-to-rtos-solution-to-part-5-freertos-queue-example/72d2b361f7b94e0691d947c7c29a03c9

--- threads_RTOS_practice/src/main.c
+++ threads_RTOS_practice/src/main.c
@@ -13,98 +13,120 @@
 #include <stdlib.h>
 #include <drivers/uart.h>
 
+#define LED0_NODE DT_ALIAS(led0)
+
+#if !DT_NODE_HAS_STATUS(LED0_NODE, okay)
+#error "Unsupported board: led0 devicetree alias is not defined"
+#endif
+
+struct led {
+	struct gpio_dt_spec spec;
+	const char *gpio_pin_name;
+};
+
+static const struct led led0 = {
+	.spec = GPIO_DT_SPEC_GET_OR(LED0_NODE, gpios, {0}),
+	.gpio_pin_name = DT_PROP_OR(LED0_NODE, label, ""),
+};
+
+
 #define STACK_SIZE 1024
 #define PRIORITY 7
-
-// Settings
-static const uint8_t buf_len = 255;
-
-// Globals
-static char *msg_ptr = NULL;
-static volatile uint8_t msg_flag = 0;
 
 K_THREAD_STACK_DEFINE(stack_area1, STACK_SIZE);
 K_THREAD_STACK_DEFINE(stack_area2, STACK_SIZE);
 
+// Settings
+static const uint8_t buf_len = 255;     // Size of buffer to look for command
+static const char command[] = "delay "; // Note the space!
+
+// Message struct: used to wrap strings (not necessary, but it's useful to see
+// how to use structs here)
+
+struct LEDMQ {  
+  int led_delay;
+};
+
+K_MSGQ_DEFINE(delay_msgq, sizeof(struct LEDMQ), 20, 4);
 
 static const struct device *uart_dev = DEVICE_DT_GET(DT_CHOSEN(zephyr_console));
 
-// Task: print message whenever flag is set and free buffer
-void printMessage() {
+// Task: flash LED based on delay provided, notify other task every 100 blinks
+void blinkLED() {
+
+  struct LEDMQ led_msg;
+  led_msg.led_delay = 500;
+
+  // Loop forever
   while (1) {
-
-    // Wait for flag to be set and print message
-    if (msg_flag == 1) {
-      printk("%s", msg_ptr);
-
-      // Give amount of free heap memory (uncomment if you'd like to see it)
-     printk("Free heap (bytes): %d\r\n", );
-//      printk(xPortGetFreeHeapSize());
-
-      // Free buffer, set pointer to null, and clear flag
-      k_free(msg_ptr);
-      msg_ptr = NULL;
-      msg_flag = 0;
-    }
-    else {
-      k_yield();
-    }
+    k_msgq_get(&delay_msgq, &led_msg, K_NO_WAIT);
+    // Blink
+    gpio_pin_toggle(led0.spec.port, led0.spec.pin);
+    k_msleep(led_msg.led_delay);
   }
 }
 
-void readSerial() 
+// Task: command line interface (CLI)
+void doCLI() 
 {
-
-  char recv_char;
+  char c;
   char buf[buf_len];
   uint8_t idx = 0;
+  uint8_t cmd_len = strlen(command);
+  struct LEDMQ led_msg;
 
   // Clear whole buffer
   memset(buf, 0, buf_len);
-
-
 
   // Loop forever
   while (1) {
 
     // Read cahracters from serial
     
-      while (uart_poll_in(uart_dev, &recv_char) < 0) {
-				/* Allow other thread/workqueue to work. */
-				k_yield();
-		}
-
-      // Store received character to buffer if not over buffer limit
-      if (idx < buf_len - 1) {
-        buf[idx] = recv_char;
-        idx++;
-      }
-
-      // Create a message buffer for print task
-      if (recv_char == '\n') {
-
-        // The last character in the string is '\n', so we need to replace
-        // it with '\0' to make it null-terminated
-        buf[idx - 1] = '\0';
-
-        // Try to allocate memory and copy over message. If message buffer is
-        // still in use, ignore the entire message.
-        if (msg_flag == 0) {
-          msg_ptr = (char *)k_malloc(idx * sizeof(char));
-
-          // Copy message
-          memcpy(msg_ptr, buf, idx);
-
-          // Notify other task that message is ready
-          msg_flag = 1;
-        }
-
-        // Reset receive buffer and index counter
-        memset(buf, 0, buf_len);
-        idx = 0;
-      }
+    while (uart_poll_in(uart_dev, &c) < 0) {
+        /* Allow other thread/workqueue to work. */
+        k_yield();
     }
-  
+
+    // Store received character to buffer if not over buffer limit
+    if (idx < buf_len - 1) {
+      buf[idx] = c;
+      idx++;
+    }
+
+    // Print newline and check input on 'enter'
+    if ((c == '\n') || (c == '\r')) {
+
+      // Print newline to terminal
+      printk("\r\n");
+      printk("%s", buf);
+
+      // Check if the first 6 characters are "delay "
+      if (memcmp(buf, command, cmd_len) == 0) {
+        printk("update LED\r\n");
+        // Convert last part to positive integer (negative int crashes)
+        char* tail = buf + cmd_len;
+        led_msg.led_delay = atoi(tail);
+        led_msg.led_delay = abs(led_msg.led_delay);
+        printk("Led delay: %d\r\n", led_msg.led_delay);
+        // Send integer to other task via queue
+        // k_queue_append(&delay_queue, &led_delay);
+        /* send data to consumers */
+        while (k_msgq_put(&delay_msgq, &led_msg, K_NO_WAIT) != 0) {
+            /* message queue is full: purge old data & try again */
+            k_msgq_purge(&delay_msgq);
+        }
+      }
+
+      // Reset receive buffer and index counter
+      memset(buf, 0, buf_len);
+      idx = 0;
+
+    // Otherwise, echo character back to serial terminal
+    } else {
+      printk("%c\r\n",c);
+    }
+  }  
 }
 
 void main(void)
@@ -116,17 +138,29 @@ void main(void)
 		return;
 	}
 
+  if (!device_is_ready(led0.spec.port)) {
+		printk("Error: %s device is not ready\n", led0.spec.port->name);
+		return;
+	}
+
+	ret = gpio_pin_configure_dt(&led0.spec, GPIO_OUTPUT);
+	if (ret != 0) {
+		printk("Error %d: failed to configure pin %d (LED '%s')\n",
+			ret, led0.spec.pin, led0.gpio_pin_name);
+		return;
+	}
+
 	struct k_thread thread1, thread2;
 
 	k_thread_create(&thread1, stack_area1,
                                  K_THREAD_STACK_SIZEOF(stack_area1),
-                                 readSerial,
+                                 blinkLED,
                                  NULL, NULL, NULL,
                                  PRIORITY, 0, K_NO_WAIT);
 
 	k_thread_create(&thread2, stack_area2,
                                  K_THREAD_STACK_SIZEOF(stack_area2),
-                                 printMessage,
+                                 doCLI,
                                  NULL, NULL, NULL,
                                  PRIORITY, 0, K_NO_WAIT);
 }
