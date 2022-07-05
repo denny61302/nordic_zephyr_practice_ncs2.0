fb65c7b65c3ccaa92feffa5ad34d8cd53ad2fdd0
denny61302 <denny61302@gmail.com>
2022-07-05 14:06:15 +0930

    PWM motor test
    512Hz
    
    speed up and then speed down

--- blinky_pwm/src/main.c
+++ blinky_pwm/src/main.c
@@ -17,7 +17,7 @@
 static const struct pwm_dt_spec pwm_led0 = PWM_DT_SPEC_GET(DT_ALIAS(pwm_led0));
 static const struct pwm_dt_spec pwm_led1 = PWM_DT_SPEC_GET(DT_ALIAS(pwm_led1));
 
-#define MIN_PERIOD PWM_SEC(1U) / 128U
+#define MIN_PERIOD PWM_SEC(1U) / 512U
 #define MAX_PERIOD PWM_SEC(1U)
 
 void main(void)
@@ -26,6 +26,7 @@ void main(void)
 	uint32_t period;
 	uint8_t dir = 0U;
 	int ret;
+	float factor=0;
 
 	printk("PWM-based blinky\n");
 
@@ -75,24 +76,38 @@ void main(void)
 	printk("Done calibrating; maximum/minimum periods %u/%lu usec\n",
 	       max_period, MIN_PERIOD);
 
-	period = max_period;
-	while (1) {
-		ret = pwm_set_dt(&pwm_led0, period, period / 2U);
-		ret = pwm_set_dt(&pwm_led1, period, period / 2U);
+	period = MIN_PERIOD;
+	
+	while (factor <= 10) {
+		
+		
+		printf("factor %.2f\r\n", (float)(factor / 10));
+		
+		ret = pwm_set_dt(&pwm_led0, period, period * (factor / 10));
+		if (ret) {
+			printk("Error %d: failed to set pulse width\n", ret);
+			return;
+		}
+		ret = pwm_set_dt(&pwm_led1, period, period * (factor / 10));
 		if (ret) {
 			printk("Error %d: failed to set pulse width\n", ret);
 			return;
 		}
 
-		period = dir ? (period * 2U) : (period / 2U);
-		if (period > max_period) {
-			period = max_period / 2U;
-			dir = 0U;
-		} else if (period < MIN_PERIOD) {
-			period = MIN_PERIOD * 2U;
-			dir = 1U;
+		if(factor == 10) 
+		{			
+			dir = 1;
+		} else if (factor == 0)
+		{
+			dir = 0;
 		}
-
-		k_sleep(K_SECONDS(4U));
+		if (dir == 0) {
+			factor = factor + 1;
+		}
+		else {
+			factor = factor - 1;
+		}
+		
+		k_sleep(K_SECONDS(2U));
 	}
 }
