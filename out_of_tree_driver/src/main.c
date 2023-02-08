57779027e5c9eb1476d5d8fc811beee5435219a7
denny61302 <denny61302@gmail.com>
2023-02-09 10:09:06 +1030

    learn how to use out of tree driver
    Driver from https://github.com/Riphiphip/zephyr-uart-driver-example

---
+++ out_of_tree_driver/src/main.c
@@ -0,0 +1,35 @@
+
+#include <zephyr/zephyr.h>
+#include <zephyr/devicetree.h>
+#include <zephyr/device.h>
+
+#include "api.h"
+
+struct counter
+{
+    int strings;
+    int overflows;
+};
+
+static struct counter tracker = {0};
+
+void peripheral_callback(const struct device *dev, char *data, size_t length, bool is_string, void *user_data)
+{
+    struct counter *c = (struct counter *)user_data;
+    if (is_string)
+    {
+        printk("Recieved string \"%s\"\n", data);
+        c->strings++;
+    } else {
+        printk("Buffer full. Recieved fragment %.*s\n", length, data);
+        c->overflows++;
+    }
+    printk("Strings: %d\nOverflows: %d\n", c->strings, c->overflows);
+}
+
+
+void main(void)
+{
+    const struct device *dev = device_get_binding(DT_LABEL(DT_NODELABEL(my_device)));
+    my_uart_set_callback(dev, peripheral_callback, &tracker);
+}
