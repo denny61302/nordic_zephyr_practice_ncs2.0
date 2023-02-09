67801adda70832eabf612cb908c57b84971ce86f
denny61302 <denny61302@gmail.com>
2023-02-09 17:08:19 +1030

    add triangle wave generator
    enable sprintf & printf

--- hello_world/src/main.c
+++ hello_world/src/main.c
@@ -5,8 +5,36 @@
  */
 
 #include <zephyr/zephyr.h>
+// #include <stdio.h>
+
+static float counter = 0.0;
+
+static void repeating_timer_callback(struct k_work *dummy){
+	char str[100];
+	char null = '\0';
+	counter++;
+	if(counter > 100){
+		counter = 0;
+	}
+	sprintf(str,"%.2f", counter);
+	// printk("Timer counter %.2f\n", counter);
+	printf(str);
+	printf("%c", null);
+}
+
+K_WORK_DEFINE(repeating_timer_work, repeating_timer_callback);
+
+void repeating_timer_handler(struct k_timer *dummy)
+{
+    k_work_submit(&repeating_timer_work);
+}
+
+K_TIMER_DEFINE(my_timer, repeating_timer_handler, NULL);
 
 void main(void)
 {
-	printk("Hello World! %s\n", CONFIG_BOARD);
+	printf("Hello World! %s\n", CONFIG_BOARD);
+
+	k_timer_init(&my_timer, repeating_timer_handler, NULL);
+	k_timer_start(&my_timer, K_NO_WAIT, K_MSEC(100));	
 }
